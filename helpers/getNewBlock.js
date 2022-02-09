11e4bfd83c5663594e79fff4f69d77780893c928
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2022-02-09 11:36:23 +0200

    fix newBlock v.0.2

--- helpers/getNewBlock.js
+++ helpers/getNewBlock.js
@@ -3,26 +3,26 @@ const { getNumberBlock, getBlockByNumber } = require("../api/service");
 const { Transactions } = require("../schemas/mongoose/transactions");
 const sleepTimeout = require("./sleep");
 
-function getNewBlock() {
-  setInterval(async () => {
-    let numberBlock = await getNumberBlock();
-    await sleepTimeout(5000);
-    const block = await getBlockByNumber(numberBlock);
-    await Transactions.updateMany({}, { $inc: { confirmayions: 1 } });
-    const currentBlock = block.transactions.map((data) => {
-      return {
-        blockNumber: parseInt(data.blockNumber),
-        id: data.hash,
-        sender: data.from,
-        recipient: data.to,
-        confirmayions: 0,
-        date: format(new Date(), "MMM-dd-yyyy"),
-        value: parseInt(data.value),
-        fee: parseInt(data.nonce),
-      };
-    });
-    await Transactions.insertMany(currentBlock);
-  }, 5000);
+async function getNewBlock() {
+  // setInterval(async () => {
+  let numberBlock = await getNumberBlock();
+  await sleepTimeout(5000);
+  const block = await getBlockByNumber(numberBlock);
+  await Transactions.updateMany({}, { $inc: { confirmayions: 1 } });
+  const currentBlock = block.transactions.map((data) => {
+    return {
+      blockNumber: parseInt(data.blockNumber),
+      id: data.hash,
+      sender: data.from,
+      recipient: data.to,
+      confirmayions: 0,
+      date: format(new Date(), "MMM-dd-yyyy"),
+      value: parseInt(data.value),
+      fee: parseInt(data.nonce),
+    };
+  });
+  await Transactions.insertMany(currentBlock);
+  // }, 5000);
 }
 
 module.exports = getNewBlock;
